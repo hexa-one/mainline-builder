b9c634f52d3375988bc774cd4ed5a4ff224991a0
Quinten <58103738+hexa-one@users.noreply.github.com>
2021-05-11 17:16:59 +0200

    Create main.py

---
+++ tools/submodule/main.py
@@ -0,0 +1,5 @@
+import os
+
+os.system('git config user.name github-actions')
+os.system('git config user.email "action@github.com"')
+os.system('git clone https://github.com/hexa-one/ubuntumainline.git && cd ubuntumainline/mainline-builder && git pull && cd .. && git add --all && git commit -m "Update submodule" && git push')
