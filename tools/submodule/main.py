6304f6723c93f10d7d0f90e2dda18366de613b12
Quinten <58103738+hexa-one@users.noreply.github.com>
2021-05-11 17:23:06 +0200

    add debug

--- tools/submodule/main.py
+++ tools/submodule/main.py
@@ -2,4 +2,4 @@ import os
 
 os.system('git config user.name github-actions')
 os.system('git config user.email "action@github.com"')
-os.system('git clone https://github.com/hexa-one/ubuntumainline.git && cd ubuntumainline/mainline-builder && git pull && cd .. && git add --all && git commit -m "Update submodule" && git push')
+os.system('git clone https://github.com/hexa-one/ubuntumainline.git && cd ubuntumainline/mainline-builder && ls -lah && git pull && cd .. && git add --all && git commit -m "Update submodule" && git push')
