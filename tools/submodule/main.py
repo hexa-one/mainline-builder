51cb0f6da923128aa185442915a67d9aa8d03acf
hexa-one <oproo48ly@relay.firefox.com>
2021-05-23 12:02:27 +0000

    try to fix git push

--- tools/submodule/main.py
+++ tools/submodule/main.py
@@ -1,5 +1,5 @@
 import os
 
-os.system('export GITHUB_TOKEN=$token')
-os.system('git auth $GITHUB_TOKEN')
-os.system('git config --global user.email "action@github.com" && git config --global user.name "github-actions" && git clone --recurse-submodules https://github.com/hexa-one/ubuntumainline.git && cd ubuntumainline/mainline-builder && git pull origin main && cd .. && git add --all && git commit -m "Update submodule" && git push')
+os.system('export GITHUB_TOKEN=$GITHUB_TOKEN')
+os.system('export GITHUB_ACTOR=$GITHUB_ACTOR')
+os.system('git config --global user.email "action@github.com" && git config --global user.name "github-actions" && git clone --recurse-submodules https://github.com/hexa-one/ubuntumainline.git && cd ubuntumainline/mainline-builder && git pull origin main && cd .. && git add --all && git commit -m "Update submodule" && git push "https://$GITHUB_ACTOR:$GITHUB_TOKEN@github.com/hexa-one/ubuntumainline/.git" main')
