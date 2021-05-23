19ff577324f8c10bf7fb90269ba0692a1cde7460
hexa-one <oproo48ly@relay.firefox.com>
2021-05-23 11:44:09 +0000

    try to git clone

--- tools/submodule/main.py
+++ tools/submodule/main.py
@@ -1,3 +1,3 @@
 import os
 
-os.system('git config --global user.email "action@github.com" && git config --global user.name "github-actions" && git clone --recurse-submodules git@github.com/hexa-one/ubuntumainline.git && cd ubuntumainline/mainline-builder && git pull origin main && cd .. && git add --all && git commit -m "Update submodule" && git push')
+os.system('git config --global user.email "action@github.com" && git config --global user.name "github-actions" && git clone --recurse-submodules git@github.com:hexa-one/ubuntumainline.git && cd ubuntumainline/mainline-builder && git pull origin main && cd .. && git add --all && git commit -m "Update submodule" && git push')
