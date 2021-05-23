e96039d2caffa0654ae8d67fa279dcfb2502884c
hexa-one <oproo48ly@relay.firefox.com>
2021-05-23 11:33:43 +0000

    fix git not cloning submodules

--- tools/submodule/main.py
+++ tools/submodule/main.py
@@ -2,4 +2,4 @@ import os
 
 os.system('git config --global user.name github-actions')
 os.system('git config --global user.email "action@github.com"')
-os.system('git clone https://github.com/hexa-one/ubuntumainline.git && cd ubuntumainline/mainline-builder && ls -lah && git pull && cd .. && git add --all && git commit -m "Update submodule" && git push')
+os.system('git clone --recurse-submodules https://github.com/hexa-one/ubuntumainline.git && cd ubuntumainline/mainline-builder && ls -lah && git pull && cd .. && git add --all && git commit -m "Update submodule" && git push')
