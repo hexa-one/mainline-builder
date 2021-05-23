443e152b72cd0204454b1ce2b40c95625a453736
hexa-one <oproo48ly@relay.firefox.com>
2021-05-23 11:54:00 +0000

    setup git auth

--- tools/submodule/main.py
+++ tools/submodule/main.py
@@ -1,6 +1,5 @@
 import os
 
 os.system('export GITHUB_TOKEN=$token')
-os.system('git remote set-url upstream https://$token:x-oauth-basic@github.com/hexa-one/mainline-builder.git')
-os.system('git remote set-url origin https://$token:x-oauth-basic@github.com/hexa-one/mainline-builder.git')
+os.system('git auth $GITHUB_TOKEN')
 os.system('git config --global user.email "action@github.com" && git config --global user.name "github-actions" && git clone --recurse-submodules https://github.com/hexa-one/ubuntumainline.git && cd ubuntumainline/mainline-builder && git pull origin main && cd .. && git add --all && git commit -m "Update submodule" && git push')
