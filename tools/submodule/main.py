d1f660bb13a73eab13fff1a24b52a27652b5e33d
hexa-one <oproo48ly@relay.firefox.com>
2021-05-23 11:50:45 +0000

    try fix github not pushing

--- tools/submodule/main.py
+++ tools/submodule/main.py
@@ -1,3 +1,6 @@
 import os
 
-os.system('git config --global user.email "action@github.com" && git config --global user.name "github-actions" && git clone --recurse-submodules git@github.com:hexa-one/ubuntumainline.git && cd ubuntumainline/mainline-builder && git pull origin main && cd .. && git add --all && git commit -m "Update submodule" && git push')
+os.system('export GITHUB_TOKEN=$token')
+os.system('git remote set-url upstream https://$token:x-oauth-basic@github.com/hexa-one/mainline-builder.git')
+os.system('git remote set-url origin https://$token:x-oauth-basic@github.com/hexa-one/mainline-builder.git')
+os.system('git config --global user.email "action@github.com" && git config --global user.name "github-actions" && git clone --recurse-submodules https://github.com/hexa-one/ubuntumainline.git && cd ubuntumainline/mainline-builder && git pull origin main && cd .. && git add --all && git commit -m "Update submodule" && git push')
