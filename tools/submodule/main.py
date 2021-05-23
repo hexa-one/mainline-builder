112eac4efdd6d2b32ba4f77589bc7706ae973dd6
hexa-one <oproo48ly@relay.firefox.com>
2021-05-23 11:39:08 +0000

    combine git config

--- tools/submodule/main.py
+++ tools/submodule/main.py
@@ -1,5 +1,3 @@
 import os
 
-os.system('git config --global user.name github-actions')
-os.system('git config --global user.email "action@github.com"')
-os.system('git clone --recurse-submodules https://github.com/hexa-one/ubuntumainline.git && cd ubuntumainline/mainline-builder && git pull origin main && cd .. && git add --all && git commit -m "Update submodule" && git push')
+os.system('git config --global user.email "action@github.com" && git config --global user.name "github-actions" && git clone --recurse-submodules https://github.com/hexa-one/ubuntumainline.git && cd ubuntumainline/mainline-builder && git pull origin main && cd .. && git add --all && git commit -m "Update submodule" && git push')
